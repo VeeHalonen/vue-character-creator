86e393789d88cb7e2914dbc6d6caad101d9edbab
Veera Halonen <veera.halonen@sofiadigital.fi>
2021-05-12 11:06:38 +0300

    Add second test for generateName

--- src/helpers/__tests__/index.test.js
+++ src/helpers/__tests__/index.test.js
@@ -2,9 +2,19 @@ import { generateName } from '../index'
 
 describe("Name generator function", () => {
     it('generates a string of length >0', () => {
-  
       const name = generateName();
       expect(typeof name).toBe("string");
       expect(name.length).toBeGreaterThan(0);
+    })
+
+    it('generates a string starting with given parameter', () => {
+      const starts = "Abc";
+      const name = generateName(starts);
+      let exp = '^' + starts;
+      let re = new RegExp(exp);
+
+      expect(name).toEqual(
+        expect.stringMatching(re)
+      )
     })
   });
